84afd3e36d1969d31c0d66a11f2620d1622f2945
truonghedspi <truong.hn.1994@gmail.com>
2019-08-18 23:46:22 +0700

    backtrack

--- tlq.py
+++ tlq.py
@@ -1,13 +1,14 @@
 import itertools
-from copy import copy, deepcopy
+import copy
 
 row = 10
 col = 4
-max_elm_equal = 2
+max_elm_equal = 3
 
 combines = list(itertools.combinations(list(range(2, row + 1)), col - 1))
 
 matrix = [[0 for x in range(col)] for y in range(row)]
+result = []
 
 def init_matrix(m):
     i = 0
@@ -25,60 +26,44 @@ def fill_matrix(matrix, combine):
         init_matrix(matrix)
         update_first_row(matrix, combine)
         num_rows = len(matrix)
-        num_cols = len(matrix[0])
 
         for row in range(1, num_rows):
-                for col in range(1, num_cols):
-                        selectables = set(range(1, num_rows+1))
-                        selected_in_cols = set([row[col] for row in matrix[0:row]])
-                        selected_in_rows = set(matrix[row][0:col])
-                        selectables = selectables - (selected_in_cols | selected_in_rows)
+                update_matrix(matrix, combine, row, 1)
 
-                        value = 0
-                        for selectable in selectables:
-                                selected_in_rows.add(selectable)
-                                valid = True
-                                for selected_row in range(0, row):
-                                        intersection = selected_in_rows & set(matrix[selected_row])
-                                        if len(intersection) > max_elm_equal:
-                                                valid = False
-                                                break
-                                if valid:
-                                        value = selectable
-                                        break
-                                selected_in_rows.discard(selectable)
-                        if value == 0:
-                                return
-                        matrix[row][col] = value
 
-def update_matrix(matrix, row, col):
+def update_matrix(matrix, combine, row, col):
         num_rows = len(matrix)
         num_cols = len(matrix[0])
-        if num_rows == row or num_cols == col:
+        if row == num_rows-1 and col == num_cols:
+                result.append(copy.deepcopy(matrix))
+
+        if col == num_cols:
                 return
         selectables = set(range(1, num_rows+1))
-                        selected_in_cols = set([row[col] for row in matrix[0:row]])
-                        selected_in_rows = set(matrix[row][0:col])
-                        selectables = selectables - (selected_in_cols | selected_in_rows)
+        selected_in_cols = set([row[col] for row in matrix[0:row]])
+        selected_in_rows = set(matrix[row][0:col])
+        selectables = selectables - (selected_in_cols | selected_in_rows)
 
-                        value = 0
-                        for selectable in selectables:
-                                selected_in_rows.add(selectable)
-                                valid = True
-                                for selected_row in range(0, row):
-                                        intersection = selected_in_rows & set(matrix[selected_row])
-                                        if len(intersection) > max_elm_equal:
-                                                valid = False
-                                                break
-                                if valid:
-                                        value = selectable
-                                        break
-                                selected_in_rows.discard(selectable)
-                        if value == 0:
-                                return
-                        matrix[row][col] = value
-fill_matrix(matrix, combines[0])
-print(matrix)
+        for selectable in sorted(selectables, reverse=True):
+                selected_in_rows.add(selectable)
+                valid = True
+                for selected_row in matrix[0:row]:
+                        intersection = selected_in_rows & set(selected_row)
+                        if len(intersection) > max_elm_equal:
+                                valid = False
+                                break
+                                     
+                selected_in_rows.discard(selectable)                
+                if valid:
+                        matrix[row][col] = selectable
+                else:
+                        matrix[row][col] = 0
+                update_matrix(matrix, combine, row, col + 1)
+
+for c in combines:
+        fill_matrix(matrix, c)
+
+print('\n'.join('{}: {}'.format(*k) for k in enumerate(result)))
 
 
 
